7a3998555a32ee2102f7175e29b912264d481d2a
Yogi Sugigik <67528759+yogisugigik@users.noreply.github.com>
2021-12-02 22:12:36 +0700

    Update loginstudent.js

--- back/loginstudent.js
+++ back/loginstudent.js
@@ -1,51 +1,132 @@
-import React from 'react';
-import {Text, View, Image, TextInput} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import {
+  responsiveHeight,
+  responsiveWidth,
+  responsiveFontSize,
+} from 'react-native-responsive-dimensions';
 
-const App = () => {
+export default function App() {
+  const [nim, setNim] = useState('');
+  const [password, setPassword] = useState('');
+
+  var state = 0;
+  const onClick = () => {
+    var nim = 203307020;
+    if (state % 2 == 0) {
+      state++;
+      setNim(nim);
+      setPassword(nim);
+    } else if (state % 2 == 1) {
+      setNim('');
+      setPassword('');
+    }
+  };
+
   return (
-    <View>
-      <View style={{width: 80, height: 80, backgroundColor: '#0abde3'}} />
-      <View style={{width: 80, height: 80, backgroundColor: 'blue'}} />
-      <Text>Hallo</Text>
-      <Text>Semua</Text>
-      <Hudoro />
-      <Text>Loh</Text>
-      <Text>{average()}</Text>
-      <Photo />
-      <TextInput />
+    <View style={styles.viewContainer}>
+      <View style={styles.cardColour}>
+        <View style={styles.viewWrapper}>
+          <Image
+            source={{
+              uri: 'http://student.pnm.ac.id/assets/img/logo-dark.png',
+            }}
+            style={styles.imageLogin}
+          />
+        </View>
+        <View style={[styles.inCard, styles.elevation]}>
+          <TextInput
+            placeholder="NPM"
+            keyboardType="number-pad"
+            style={styles.textInput}>
+            {nim}
+          </TextInput>
+          <TextInput
+            placeholder="Password"
+            style={styles.textInput}
+            secureTextEntry>
+            {password}
+          </TextInput>
+          <TouchableOpacity onPress={onClick}>
+            <View style={styles.viewButton}>
+              <Text style={styles.textLogin}>LOGIN</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
-};
-
-const Hudoro = () => {
-  return <Text>Hudoro Mangku Udoro</Text>;
-};
-
-const Photo = () => {
-  return (
-    <Image
-      source={{uri: 'https://placeimg.com/100/100/tech'}}
-      style={{width: 100, height: 100}}
-    />
-  );
-};
-
-const Laheh = () => {
-  return (
-    <Image
-      source={{uri: 'https://placeimg.com/640/480/nature'}}
-      style={{width: 640, height: 480}}
-    />
-  );
-};
-
-function average() {
-  let int1, int2, hasil;
-  int1 = 100;
-  int2 = 200;
-  hasil = int1 + int2;
-  return hasil;
 }
 
-export default App;
+const styles = StyleSheet.create({
+  viewContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  textInput: {
+    width: responsiveWidth(81),
+    height: responsiveHeight(7),
+    marginVertical: responsiveHeight(2.0),
+    borderColor: '#ebebeb',
+    alignSelf: 'center',
+    borderBottomWidth: 1,
+    padding: 10,
+  },
+  viewButton: {
+    width: responsiveWidth(25),
+    height: responsiveHeight(5),
+    alignItems: 'center',
+    backgroundColor: '#03a9f4',
+    justifyContent: 'center',
+    alignSelf: 'flex-end',
+    borderRadius: 3,
+    marginTop: responsiveHeight(3.0),
+    marginRight: responsiveHeight(3.0),
+  },
+  textLogin: {
+    fontWeight: '500',
+    color: '#ebebeb',
+    fontSize: responsiveFontSize(2.2),
+  },
+  imageLogin: {
+    marginTop: 50,
+    width: 250,
+    height: 50,
+    resizeMode: 'contain',
+  },
+  viewWrapper: {
+    alignItems: 'center',
+  },
+  textTitle: {
+    fontSize: responsiveFontSize(2.2),
+    fontWeight: 'bold',
+    color: '#43B2EC',
+  },
+  cardColour: {
+    backgroundColor: '#F3F2F2',
+    paddingBottom: 10,
+  },
+  inCard: {
+    backgroundColor: '#FFFFFF',
+    paddingBottom: 20,
+    margin: 13,
+    paddingLeft: 10,
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+
+  elevation: {
+    elevation: 8,
+    shadowColor: '#161516',
+  },
+});
